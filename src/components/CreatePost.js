bb2bee7fae6bae4ca928cca86ce54fa9bce71777
Roman Danilov <r.danylov@gmail.com>
2020-05-23 18:18:11 +0300

    Create Post

---
+++ src/components/CreatePost.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+function CreatePost({ user, handleAddPost}) {
+    const [content, setContent] = React.useState("")
+    const [image, setImage] = React.useState(null)
+
+    const inputImgRef = React.useRef()
+
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (image || content) {
+            const post = { content, image, user };
+            handleAddPost(post);
+        // const newPosts = [post, ...posts];
+        // setPosts(newPosts);
+        setContent('')
+        inputImgRef.current.value = ''
+        setImage('')
+        }
+    }
+
+    
+    return (
+        <div >
+            <hr />
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    onChange={e => setContent(e.target.value)}
+                    value={content}
+                />
+                <input
+                    type="file"
+                    onChange={e => setImage(e.target.files[0])}
+                    ref={inputImgRef}
+                    />
+                <button type='submit'>submit</button>
+            </form>
+            {/* <p>{content}</p> */}
+            {/* {image && <img src={URL.createObjectURL(image)} alt="IMG"
+                style={{ height: 100, width: 200, objectFit: "cover" }} />} */}
+
+        </div>
+    );
+}
+
+export default CreatePost;
