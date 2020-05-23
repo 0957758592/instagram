bab447c70f8fc60da363113f5d86a1b05472a3f0
Roman Danilov <r.danylov@gmail.com>
2020-05-23 18:18:26 +0300

    create Header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header({ user, setUser }) {
+    
+
+    function handleClick(e) {
+        setUser("")
+    }
+
+    return (
+        <div >
+            Welcome {user}
+            <button onClick={handleClick}>Logout</button>
+        </div>
+    );
+}
+
+export default Header;
